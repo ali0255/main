69f9fa92b0da3951612154054719e5fd2bc4877e
Yousef Moghadam <111203925+mrthemgi@users.noreply.github.com>
2024-06-24 03:56:29 +0330

    fix tokenable In API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', 'App\Http\Controllers\LoginController@store');
+Route::get('/get-me', 'App\Http\Controllers\LoginController@store');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
